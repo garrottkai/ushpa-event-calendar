6637592adaf2f5a57a3ef0540538ce2eeff16ea3
Kai Garrott <garrottkai@gmail.com>
2016-04-14 09:18:42 -0600

    Expand index page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="author" content="Kai Garrott" />
+		<meta name="description" content="Data design landing page" />
+		<title>Data Design Project</title>
+	</head>
+	<body>
+		<h1>Data Design Project</h1>
+		<p>Welcome to my analysis of the data structure of an interactive web page: The United States Hang Gliding and Paragliding Association <a href="https://www.ushpa.org/page/calendar">event calendar</a>.</p>
+		<br>
+		<p>Persona & Use Cases</p>
+		<p>Data Relationships</p>
+	</body>
+</html>
