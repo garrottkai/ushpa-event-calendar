8475788d904440949312eef14691119aa9a9846a
Kai Garrott <garrottkai@gmail.com>
2016-04-14 09:43:25 -0600

    Add style sheet

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
 		<meta name="author" content="Kai Garrott" />
 		<meta name="description" content="Data design landing page" />
 		<title>Data Design Project</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
 	</head>
 	<body>
+		<div class="container">
 		<h1>Data Design Project</h1>
 		<p>Welcome to my analysis of the data structure of an interactive web page:<br>The United States Hang Gliding and Paragliding Association <a href="https://www.ushpa.org/page/calendar">event calendar</a>.</p>
 		<br>
 		<p><a href="persona.php">Persona & Use Cases</a></p>
 		<p><a href="relationships.php">Data Relationships</a></p>
+		</div>
 	</body>
 </html>
