b0bf7ed575a2ea47fa2a13bf91170fdff66d1cd1
Kai Garrott <garrottkai@gmail.com>
2016-04-14 10:57:11 -0600

    Add persona characteristics

--- persona.php
+++ persona.php
@@ -12,7 +12,32 @@
 		<header>
 			<h1>Persona</h1>
 		</header>
-		<p></p>
+		<p>
+			The intended purpose of this site is twofold: to provide a public listing of upcoming events in the U.S. paragliding community, and to offer the opportunity for USHPA members to submit new event listings for display.<br>
+		Potential users of this resource can be generalized as having many of the following characteristics:<br></p>
+			<ul>
+				<li>Age: Ranging from 20 to 70; majority 30 - 60; average 40</li>
+				<li>Gender: Largely male</li>
+				<li>Education level: Typically, some college</li>
+				<li>Work: Established in a professional career, with 10+ years of experience</li>
+				<li>Income: Typical professional salary, $40k-$100k</li>
+				<li>Reason for using this site:
+					<ul>
+						<li>To find upcoming events</li>
+						<li>To post an event announcement</li>
+					</ul>
+				</li>
+				<li>User needs to be able to:
+					<ul>
+						<li>View a list of events</li>
+						<li>Easily access detailed information about each event</li>
+						<li>Submit event information using an uncomplicated interface</li>
+					</ul>
+				</li>
+				<li>Referred to page by: Magazine article</li>
+				<li>Viewing circumstances: On desktop computer, after work</li>
+				<li>Technological experience: Casual user - little beyond browsing & email</li>
+			</ul>
 		<h2></h2>
 		<p></p>
 		</div>
