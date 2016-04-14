97afa85aff8be70539c57aa297ead32f98e6acc1
Kai Garrott <garrottkai@gmail.com>
2016-04-14 11:34:28 -0600

    Add persona paragraph

--- persona.php
+++ persona.php
@@ -12,6 +12,7 @@
 		<header>
 			<h1>Persona</h1>
 		</header>
+		<h3>General user profile</h3>
 		<p>
 			The intended purpose of this site is twofold: to provide a public listing of upcoming events in the U.S. paragliding community, and to offer the opportunity for USHPA members to submit new event listings for display.<br><br>
 		Potential users of this resource can be generalized as having many of the following characteristics:<br></p>
@@ -36,10 +37,13 @@
 				</li>
 				<li>Referred to page by: Magazine article</li>
 				<li>Viewing circumstances: On desktop computer, after work</li>
-				<li>Technological experience: Casual user - little beyond browsing & email</li>
+				<li>Technological experience: Casual computer user - little beyond browsing, email, and spreadsheets</li>
 			</ul>
-		<h2></h2>
-		<p></p>
+		<h3>Example</h3>
+		<p>Dave is a 43-year-old office manager at a local auto parts chain in Spokane. He holds a Bachelor's degree in business administration from the University of Washington, and has gradually worked his way up in the company since being hired out of college. At work, Dave uses elements of the Microsoft Office suite, including Word, PowerPoint, and Excel, to organize and communicate data within the corporate structure. He has been married for 15 years and has two children in elementary and middle school.<br>
+		Dave is a recreational paraglider pilot. He started flying early in his professional career, before he was married. Having children forced him to quit flying for a few years, but as his kids got older, he's had a bit more free time on the weekends, and he returned to the sky two years ago.<br>
+		Dave's skill level could be classified as intermediate. He has a P-4 rating, but his busy schedule prevents him from engaging with the sport at the level he did 20 years ago. Nevertheless, Dave belongs to a club, members of which he flies with at a number of sites in the area, and he enjoys regularly participating in fly-ins around the Northwest.<br>
+		</p>
 		</div>
 	</body>
 </html>
