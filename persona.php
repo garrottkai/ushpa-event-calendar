98999cd823a07bffeca2c810e3ce14e961aaa3b6
Kai Garrott <garrottkai@gmail.com>
2016-04-14 10:19:00 -0600

    Expand persona page

---
+++ persona.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="author" content="Kai Garrott" />
+		<meta name="description" content="persona and use cases" />
+		<title>Persona</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+		<header>
+			<h1>Persona</h1>
+		</header>
+		<p></p>
+		<h2></h2>
+		<p></p>
+	</body>
+</html>
