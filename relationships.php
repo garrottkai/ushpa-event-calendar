8cf8988ca879148aa6375a68155c1b4361d16847
Kai Garrott <garrottkai@gmail.com>
2016-04-18 09:08:30 -0600

    Populate data relationships page

---
+++ relationships.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="author" content="Kai Garrott" />
+		<meta name="description" content="data relationships page" />
+		<title>Data Relationships</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+		<div class="container">
+		<header>
+			<h1>Data Relationships</h1>
+		</header>	
+		<h3>Entity relationship diagram</h3>
+		<img src="/images/data-design.svg"	width="600px" alt="entity relationship diagram" />
+		</div>
+	</body>
+</html>
