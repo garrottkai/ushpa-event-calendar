566c80db5d1c6acd58bc8149fad211609406b831
Kai Garrott <garrottkai@gmail.com>
2016-04-18 09:56:36 -0600

    test

--- relationships.php
+++ relationships.php
@@ -14,6 +14,6 @@
 		</header>	
 		<h3>Entity relationship diagram</h3>
 		</div>
-		<object data="/images/data-design.svg" type="image/svg+xml"></object>
+		<object data="/images/data-design.svg" type="image/svg+xml">This is an svg</object>
 	</body>
 </html>
