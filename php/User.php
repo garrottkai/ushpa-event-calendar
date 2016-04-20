e823fbfa47133f6cd966fa973391a5f9c8970669
Kai Garrott <garrottkai@gmail.com>
2016-04-20 09:45:39 -0600

    Finish populating user entity

--- php/User.php
+++ php/User.php
@@ -1,8 +1,9 @@
 <?php
-namespace Edu\Cnm\Kgarrott\Ushpa-Event-Calendar
+namespace Edu\Cnm\Kgarrott\Ushpa-event-calendar;
 /**
  * These are variables and methods for the user entity
  *
+ * @author Kai Garrott <garrottkai@gmail.com>
  */
 
 class User implements \JsonSerializable {
@@ -60,6 +61,79 @@ class User implements \JsonSerializable {
 	 */
 	public function getUserFullName() {
 		return $this->userFullName;
+	}
+
+	/**
+	 * mutator method for user full name
+	 *
+	 * @param string $newUserFullName new value of user full name
+	 * @throws \InvalidArgumentException if $newUserFullName is not a string or is insecure
+	 * @throws \TypeError if $newUserFullName is not a string
+	 */
+	public function setUserFullName(string $newUserFullName) {
+		// verify that the user full name is secure
+		$newUserFullName = trim($newUserFullName);
+		$newUserFullName = filter_var($newUserFullName, FILTER_SANITIZE_STRING);
+		if(empty($newUserFullName) === true) {
+			throw(new \InvalidArgumentException("User full name content is empty or insecure"));
+		}
+
+		// store the user full name
+		$this->userFullName = $newUserFullName;
+	}
+
+	/**
+	 * accessor method for user email
+	 *
+	 * @return string value of user email
+	 */
+	public function getUserEmail() {
+		return $this->userEmail;
+	}
+
+	/**
+	 * mutator method for user email
+	 *
+	 * @param string $newUserEmail new value of user email
+	 * @throws \InvalidArgumentException if new user email is not a string or is insecure
+	 * @throws \TypeError if new user email is not a string
+	 */
+	public function setUserEmail(string $newUserEmail) {
+		// verify the user email is secure
+		$newUserEmail = trim($newUserEmail);
+		$newUserEmail = filter_var($newUserEmail, FILTER_SANITIZE_EMAIL);
+		if(empty($newUserEmail) === true) {
+			throw(new \InvalidArgumentException("User email is empty or insecure"));
+		}
+
+		// store the user email
+		$this->userEmail = $newUserEmail;
+	}
+
+	/**
+	 * accessor method for user phone
+	 *
+	 * @return int value of user phone
+	 */
+	public function getUserPhone() {
+		return $this->userPhone;
+	}
+
+	/**
+	 * mutator method for user phone
+	 *
+	 * @param int $newUserPhone new value of user phone
+	 * @throws \RangeException if $newUserPhone is not positive
+	 * @throws \TypeError if $newUserPhone is not an integer
+	 */
+	public function setUserPhone(int $newUserPhone) {
+		// verify the new user phone is positive
+		if($newUserPhone <= 0) {
+			throw(new \RangeException("User phone is not positive"));
+		}
+
+		// store the user phone
+		$this->userPhone = $newUserPhone;
 	}
 }
 ?>
